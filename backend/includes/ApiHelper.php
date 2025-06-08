a2cdebff74c3bc26b1cde400c5c56a6aa95f156f
Unai Muñoz <unaimunoz2024@gmail.com>
2025-06-08 11:30:46 +0200

    Update ApiHelper

--- backend/includes/ApiHelper.php
+++ backend/includes/ApiHelper.php
@@ -6,35 +6,6 @@
 class ApiHelper
 {
     /**
-     * Make a POST request to a GraphQL API
-     * 
-     * @param string $url The API URL
-     * @param array $data The GraphQL query and variables
-     * @return array The API response
-     */
-    public static function graphqlRequest($url, $data)
-    {
-        $ch = curl_init($url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-            'Accept: application/json'
-        ]);
-
-        $response = curl_exec($ch);
-
-        if (curl_errno($ch)) {
-            throw new Exception('cURL Error: ' . curl_error($ch));
-        }
-
-        curl_close($ch);
-
-        return json_decode($response, true);
-    }
-
-    /**
      * Make a GET request to a REST API
      * 
      * @param string $url The API URL
